8a78052548a0f0d4700086dd15afb5229d9e9be8
Anubhav Maurya <anubhavmauryaph@gmail.com>
2025-11-12 04:14:34 +0530

    Add username field to user schema and update user data; restore BASE_URL configuration

--- backend/data/users.js
+++ backend/data/users.js
@@ -2,17 +2,20 @@ import bcrypt from 'bcryptjs';
 
 const users = [
   {
+    username: 'admin',
     name: 'Admin User',
     email: 'admin@email.com',
     password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
+    username: 'john',
     name: 'John Doe',
     email: 'john@email.com',
     password: bcrypt.hashSync('123456', 10),
   },
   {
+    username: 'jane',
     name: 'Jane Doe',
     email: 'jane@email.com',
     password: bcrypt.hashSync('123456', 10),
